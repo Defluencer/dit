ae6aa69c4d322511c77b9f48a997225dc1711b78
Sionoi <sim.vivier@hotmail.com>
2021-04-15 11:23:00 -0400

    Events, Front-running protection & Fee change

--- eth/upala.sol
+++ eth/upala.sol
@@ -6,8 +6,11 @@ import "eth/signatures.sol";
 contract UpalaGroup {
     address private owner;
 
+    uint256 private oldScore;
     uint256 public score;
-    uint256 private fee;
+
+    uint256 private oldFee;
+    uint256 public fee;
 
     //Map address to membership
     mapping(address => bool) public memberships;
@@ -16,11 +19,18 @@ contract UpalaGroup {
 
     uint256 public gracePeriodEnd;
 
-    //TODO events???
+    event NewMembership(address member);
+    event BridgeBurned(address member);
+    event LossMembership(address member);
+    event ScoreChanged(uint256 newScore, uint256 gracePeriodEnd);
+    event FeeChanged(uint256 newFee, uint256 gracePeriodEnd);
 
-    constructor() payable {
+    constructor(uint256 startFee) payable {
+        require(startFee < msg.value, "Fee must be lower than score");
+
         owner = msg.sender;
-        fee = msg.value;
+        score = msg.value;
+        fee = startFee;
     }
 
     /// The caller address will be added as a member if the nonce & signature is valid, must also pay entree fee.
@@ -41,6 +51,7 @@ contract UpalaGroup {
         );
 
         memberships[msg.sender] = true;
+        emit NewMembership(msg.sender);
     }
 
     /// The caller can transfer it's membership to another address.
@@ -54,41 +65,81 @@ contract UpalaGroup {
     ///The caller can "explode", getting the value of score transfered to his address.
     function burnBridge() public {
         require(memberships[msg.sender], "Must be a member.");
+
+        uint256 amount = score;
+
+        if (block.timestamp <= gracePeriodEnd && oldScore > score) {
+            amount = oldScore;
+        }
+
         require(
-            address(this).balance >= score,
+            address(this).balance >= amount,
             "Must have enough in contract balance."
         );
 
         memberships[msg.sender] = false;
+        emit BridgeBurned(msg.sender);
 
-        payable(msg.sender).transfer(score);
+        payable(msg.sender).transfer(amount);
     }
 
     ///The caller can leave the group, getting the value of fee transfered to his address.
     function leaveGroup() public {
         require(memberships[msg.sender], "Must be a member.");
         require(
-            address(this).balance >= fee,
+            fee <= address(this).balance,
             "Must have enough in contract balance."
         );
 
-        memberships[msg.sender] = false;
+        uint256 amount = fee;
 
-        payable(msg.sender).transfer(score);
+        if (block.timestamp <= gracePeriodEnd && oldFee > fee) {
+            amount = oldFee;
+        }
+
+        memberships[msg.sender] = false;
+        emit LossMembership(msg.sender);
+
+        payable(msg.sender).transfer(amount);
     }
 
     //  <-- Management Functions Below -->
 
-    /// Allow the owner to change the score and trigger 1 hour grace period to protect bots from front-running attack.
-    function changeScore(uint256 new_score) public {
+    /// Allow the owner to change the score and trigger grace period.
+    function changeScore(uint256 newScore) public {
         require(msg.sender == owner, "Must be the owner");
         require(
-            block.timestamp >= gracePeriodEnd,
+            block.timestamp > gracePeriodEnd,
             "Cannot update during grace period"
         );
-        require(new_score > fee, "Score Must be higher than Fee");
+        require(newScore > fee, "Score Must be higher than Fee");
+        require(
+            newScore <= address(this).balance,
+            "Must have enough in contract balance."
+        );
 
-        gracePeriodEnd = block.timestamp + 3600; // ~ 1 hour
-        score = new_score;
+        gracePeriodEnd = block.timestamp + 86400; // ~ 1 day
+
+        oldScore = score;
+        score = newScore;
+
+        emit ScoreChanged(newScore, gracePeriodEnd);
+    }
+
+    ///Allow the onwer to change the entree fee and trigger grace period.
+    function changeFee(uint256 newFee) public {
+        require(msg.sender == owner, "Must be the owner");
+        require(
+            block.timestamp > gracePeriodEnd,
+            "Cannot update during grace period"
+        );
+        require(newFee < score, "Fee Must be lower than Score");
+
+        gracePeriodEnd = block.timestamp + 86400; // ~ 1 day
+
+        oldFee = fee;
+        fee = newFee;
+
+        emit FeeChanged(newFee, gracePeriodEnd);
     }
 }
